ab6106ebb746037b39c0767557a53274af059b9d
Carlo Gilmar <carlogilmar12@gmail.com>
2025-04-25 16:54:15 -0600

    Store all the updates in the same station process

--- lib/ex_bike/station.ex
+++ lib/ex_bike/station.ex
@@ -1,6 +1,7 @@
 defmodule ExBike.Station do
   use GenServer
   require Logger
+  @max_updates 1000
 
   defstruct [
     :id,
@@ -14,7 +15,8 @@ defmodule ExBike.Station do
     :bikes_available,
     :bikes_disable,
     :docks_available,
-    :docks_disabled
+    :docks_disabled,
+    :bike_updates
   ]
 
   def start_link(attrs) do
@@ -46,24 +48,39 @@ defmodule ExBike.Station do
   @impl true
   def handle_cast({:update_status, attrs}, station) do
     updated_station =
-      Map.merge(station, %{
-        bikes_available: attrs["num_bikes_available"],
-        bikes_disable: attrs["num_bikes_disabled"],
-        docks_available: attrs["num_docks_available"],
-        docks_disabled: attrs["num_docks_disabled"]
-      })
+      if station.bikes_available != attrs["num_bikes_available"] do
+        Logger.info(
+          "[Station Sync][#{station.id}] Station #{station.name} moved from #{station.bikes_available} to #{attrs["num_bikes_available"]} bikes availables"
+        )
 
-    if station.bikes_available != attrs["num_bikes_available"] do
-      Logger.info(
-        "[Station Sync] Station #{station.name} moved from #{station.bikes_available} to #{updated_station.bikes_available} bikes availables"
-      )
+        # Saving the update
+        new_update = %{
+          updated_at: NaiveDateTime.utc_now(),
+          previous_bikes_availables: station.bikes_available,
+          current_bikes_availables: attrs["num_bikes_available"]
+        }
 
-      Phoenix.PubSub.broadcast(
-        ExBike.PubSub,
-        "stations",
-        {:station_updated, updated_station}
-      )
-    end
+        bike_updates = [new_update | station.bike_updates] |> Enum.take(@max_updates)
+
+        updated_station =
+          Map.merge(station, %{
+            bikes_available: attrs["num_bikes_available"],
+            bikes_disable: attrs["num_bikes_disabled"],
+            docks_available: attrs["num_docks_available"],
+            docks_disabled: attrs["num_docks_disabled"],
+            bike_updates: bike_updates
+          })
+
+        Phoenix.PubSub.broadcast(
+          ExBike.PubSub,
+          "stations",
+          {:station_updated, updated_station}
+        )
+
+        updated_station
+      else
+        station
+      end
 
     {:noreply, updated_station}
   end
@@ -77,7 +94,9 @@ defmodule ExBike.Station do
       lon: station_attrs["lon"],
       name: station_attrs["name"],
       rental_methods: station_attrs["rental_methods"],
-      short_name: station_attrs["short_name"]
+      short_name: station_attrs["short_name"],
+      bikes_available: 0,
+      bike_updates: []
     }
   end
 end
