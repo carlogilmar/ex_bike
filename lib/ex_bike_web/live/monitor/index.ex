8c58d8402dd4f164dfe70d63ce733caaa1c49f80
Carlo Gilmar <carlogilmar12@gmail.com>
2025-04-27 13:52:54 -0600

    Show all the stations in the view

--- lib/ex_bike_web/live/monitor/index.ex
+++ lib/ex_bike_web/live/monitor/index.ex
@@ -1,11 +1,18 @@
 defmodule ExBikeWeb.MonitorLive.Index do
   use ExBikeWeb, :live_view
+  alias ExBike.Station
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+     assign(socket,
+       stations: fetch_all_stations()
+     )}
   end
 
-
+  defp fetch_all_stations do
+    Registry.select(StationRegistry, [{{:"$1", :_, :_}, [], [:"$1"]}])
+    |> Enum.shuffle()
+    |> Enum.map(&Station.get_station/1)
+  end
 end
-
